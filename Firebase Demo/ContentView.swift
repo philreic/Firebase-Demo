2cd010f5b7ac172ca0ad835aa8fb75473e2a5654
philreic <91380439+philreic@users.noreply.github.com>
2021-11-15 09:36:37 -0700

    Initial Commit

---
+++ Firebase Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase Demo
+//
+//  Created by Philippe Reichen on 11/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firebase Demo/Firebase_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_DemoApp.swift
+//  Firebase Demo
+//
+//  Created by Philippe Reichen on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
